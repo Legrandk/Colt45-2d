8d84a69feb57922fb51f7da31cac26f7daee1a90
Mike Blake <chikoon@gmail.com>
2013-02-25 00:50:26 +0100

    simplified canvas examples. added jose's loadJSON function to the colt45_2d class

--- src/colt45_2d.js
+++ src/colt45_2d.js
@@ -3,6 +3,8 @@ Colt45_2d = Class.extend({
     canvas  : null,
     context : null,
     
+    sheets:     {},
+    
     // global settings ---------------------------------------------------------
     config : {
         canvas : { id: 'game_canvas', w: 800, h: 600 },
@@ -29,15 +31,47 @@ Colt45_2d = Class.extend({
     },
     
     // xhr ---------------------------------------------------------------------
-    loadAtlas : function(){
-        var u = this.getConfig('url.atlas');  
+    //loadAtlas : function(){ var u = this.getConfig('url.atlas'); },
+    
+    loadSpriteSheet:    function(jsonURL){
+        this.loadJSON(jsonURL, loadSpriteSheetCB);
     },
-    loadSpriteSheet: function(){},
+    loadSpriteSheetCB:  function(){
+        var json = this.responseText;
+        //self.parseAtlasDefinition(json);
+    },
+    
+    loadJSON: function(jsonURL, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", jsonURL, true);
+        xhr.onload = callback;
+        xhr.send();
+    },
     
     // drawing -----------------------------------------------------------------
-    drawSprite : function ( image , posX, posY ) {
+    drawImage : function ( image , posX, posY ) {
         this.context.drawImage( image, posX, posY );
     },
+    
+/*
+    function drawSprite(spritename, posX, posY) {
+        for(var sheetName in gSpriteSheets) {
+            var sheet   = gSpriteSheets[sheetName];
+            var sprite  = sheet.getStats(spritename);           // see if a sprite exists
+            if(sprite === null) { continue; }                   // if not, try next sheet
+            __drawSpriteInternal(sprite, sheet, posX, posY);    // if so, draw it
+            return;
+        }
+    }
+    
+    // Draw sprites giving sprite object SpriteSheet instance, and canvas xy position.
+    function __drawSpriteInternal(spt, sheet, posX, posY) {
+        if (spt === null || sheet === null) { return; }
+        var hlf = { x: spt.cx, y: spt.cy };
+        ctx.drawImage(sheet.img, spt.x, spt.y, spt.w, spt.h, posX + hlf.x, posY + hlf.y, spt.w, spt.h);
+    }
+*/
+
     clearContext: function(){
         var w = this.getConfig('canvas.w'),
             h = this.getConfig('canvas.h');
