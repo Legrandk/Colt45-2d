8aac3282e98598c314ea58aab5259df1c808af5c
Mike Blake <chikoon@gmail.com>
2013-03-12 16:20:45 +0100

    Restructured js libs and shortened the boot file name

--- src/colt45_2d.js
+++ src/colt45_2d.js
@@ -1,123 +1,39 @@
-Colt45_2d = Class.extend({
+/**
+ * To-Do: Add closure to this loading process - mike
+ */
 
-    canvas  : null,
-    context : null,
-    
-    sheets:     {},
-    
-    // global settings ---------------------------------------------------------
-    config : {
-        canvas : { id: 'game_canvas', w: 800, h: 600 },
-        sheets : { 
-            './assets/images/grits_effects.png':'./assets/json/grits_effects.json'
-        }
-    },
-    
-    // setup ---------------------------------------------------------------------
-    init : function( params ) {
-        this.config  = this.loadConfig(params);
-        this.canvas  = this.createCanvas();        
-        this.context = this.canvas.getContext('2d');
-        //this.map     = new TILEDMapClass();                 // wouldn't this be nice?
-        this.map     = (gMap) ? gMap : new TILEDMapClass();   // pending refactor of the tiledmap class
-    },
-    loadConfig: function(params){
-        if(params!=null&&(typeof params)!='object') throw "Expected a parameters object";
-        return merge(this.config, params); // core merge function
-    },
-    createCanvas : function(){
-        var canvas      = document.createElement("canvas");
-        canvas.id       = this.getConfig('canvas.id');
-        canvas.width    = this.getConfig('canvas.w');
-        canvas.height   = this.getConfig('canvas.h');
-        document.body.appendChild( canvas );
-        return canvas;
-    },
-    
-    // xhr ---------------------------------------------------------------------
-    xhrGet: function(jsonURL, callback) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET", jsonURL, true);
-        xhr.onload = callback;
-        //xhr.onload = function() { callback(xhr);}    
-        xhr.send();
-    },
-    
-    // maps --------------------------------------------------------------------
-    loadMap: function(url, callback){
-        var that = this;
-        that.map.load(url);
-        var interval = setInterval(function(){
-            if(that.map.fullyLoaded){
-                that.map.draw(that.context);
-                clearInterval(interval);
-                try{ callback(that.map); } catch(e){ /*oh, well*/ }
-            }
-        }, 500);
-    },
-    
-    // spritesheets ------------------------------------------------------------
-    loadSpriteSheet : function(imageURL, jsonURL, callback){    
-        var img     = new Image();
-        var that    = this;
-        img.src     = imageURL;
-        img.onload  = function(){
-            that.xhrGet(jsonURL, function(obj){
-                var sheet       = new SpriteSheetClass();
-                    sheet.url   = imageURL;            // Store the URL of the spritesheet we want.
-                    sheet.img   = img;                 // Store the Image object in the img parameter.
-                sheet.parseAtlasDefinition(this.responseText);
-                that.sheets[imageURL] = sheet;
-                if(callback) { try { callback(obj); } catch(e){}; }
-            });
+var script_name = 'colt45_2d.js',
+    required_modules = [
+      './lib/udacity/core.js',
+      './lib/udacity/spritesheet.js', //requires core.js
+      './lib/udacity/xhr.js',
+      './lib/udacity/tiledmap.js',    // requires xhr.js
+      './lib/Colt45_2d.js'            //requires core.js and spritesheet.js
+    ];
 
+//////////////////////////////////////////////////////////////////
+
+function loadRequiredModule( url ) {
+    var node = document.createElement("script");
+    node.async = false;
+    node.type = 'text/javascript';
+    node.charset = 'utf-8';
+    node.src = url;
+    document.getElementsByTagName('head')[0].appendChild(node);
+}
+
+function getBaseURL() {
+    var scripts = document.getElementsByTagName('script');
+    for ( var i in scripts ) {
+        if ( scripts[i].src.indexOf(script_name) > -1 ) {
+            return scripts[i].src.replace(script_name , "");
         }
-    },
-    getSpriteSheet: function(imgUrl){
-        if(this.sheets[''+imgUrl]) { return this.sheets[''+imgUrl]; }
-        else { throw "Unknown SpriteSheet: " + imgUrl; }
-    },
-    
-    // sprites -----------------------------------------------------------------
-    getSprite: function(spritename){
-        for(var sheetName in this.sheets) {
-            var sheet   = this.sheets[sheetName];
-            var sprite  = sheet.getStats(spritename);           // see if a sprite exists
-            if(sprite === null) { continue; }
-            sprite.sheet_name = sheetName;
-            return sprite; 
-        }
-        throw "Unknown sprite: " + spritename;
-    },
-    drawSprite : function(spritename, posX, posY) {
-        var sprite = this.getSprite(spritename),
-            sheet  = this.getSpriteSheet(sprite.sheet_name);
-        this.__drawSpriteInternal(sprite, sheet, posX, posY);    // if so, draw it
-    },
-    // Draw sprites giving sprite object SpriteSheet instance, and canvas xy position.
-    __drawSpriteInternal:function(spt, sheet, posX, posY) {
-        if (spt === null || sheet === null) { return; }
-        var hlf = { x: spt.cx, y: spt.cy };
-        this.context.drawImage(sheet.img, spt.x, spt.y, spt.w, spt.h, posX + hlf.x, posY + hlf.y, spt.w, spt.h);
-    },
-    
-    // drawing -----------------------------------------------------------------
-    drawImage : function ( image , posX, posY ) { this.context.drawImage( image, posX, posY ); },
-    clearContext: function(){
-        var w = this.getConfig('canvas.w'),
-            h = this.getConfig('canvas.h');
-        this.context.clearRect(0, 0, w, h);
-    },
-    
-    // util --------------------------------------------------------------------
-    getConfig: function(n, dflt) {
-        var parts = n.split('.'), res = this.config, i;
-        for (i = 0; i < parts.length; i++) {
-            if (typeof res[parts[i]] !== 'undefined') { res = res[parts[i]]; }
-            else { return dflt; }
-        }
-        return res;
     }
-  
+    return "";
+}
 
-});
+var baseURL = getBaseURL();
+
+for (var module in required_modules ) {
+    loadRequiredModule( baseURL + "" + required_modules[module] );
+}
