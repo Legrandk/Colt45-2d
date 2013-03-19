a7ba81a00a7fb315b7e1b71efa7204647d5bc702
Mike Blake <chikoon@gmail.com>
2013-03-19 19:55:02 +0100

    camelCased instance.tileSets

--- src/lib/TiledMap.js
+++ src/lib/TiledMap.js
@@ -1,7 +1,7 @@
 var TiledMap = Class.extend({
 
     currMapData: null,              // full parsed json
-    tilesets: [],                   // tilesets stores each individual tileset from the map.json's 'tilesets' Array. See structure in parseAtlasDefinition.
+    tileSets: [],                   // tileSets stores each individual tileset from the map.json's 'tileSets' Array. See structure in parseAtlasDefinition.
     numXTiles: -1, numYTiles:-1,    // Width and height of the map in tiles from map.json's 
     tileSize: { "x": -1, "y":-1 },  // The size of each individual map tile from Map.json's tilewidth and tileheight values.  Initialized to something, rather than null (in pixels). 
     pixelSize: { "x":-1, "y":-1 },  // The size of the entire map, calculated using map.json's numXTiles, numYTiles, and tileSize parameters. Initialized to something, rather than null (in pixels).
@@ -28,14 +28,14 @@ var TiledMap = Class.extend({
         that.tileSize.y     = mdata.tileheight;
         that.pixelSize.x    = that.numXTiles * that.tileSize.x;
         that.pixelSize.y    = that.numYTiles * that.tileSize.y;
-        for(var i = 0; i < mdata.tilesets.length; i++) { // Load each tileset as an Image
+        for(var i = 0; i < mdata.tilesets.length; i++) { // Load each tileset master image
             var tsdata = mdata.tilesets[i],
                 img     = new Image();
             img.onload = function () {
                 that.imgLoadCount++;
                 if (that.imgLoadCount === mdata.tilesets.length) { that.fullyLoaded = true; }
             };
-            img.src = tsdata.image;
+            img.src = tsdata.image; // url of the the tileset's master image
             var ts = {
                     "firstgid":     tsdata.firstgid,
                     "image":        img,
@@ -45,44 +45,37 @@ var TiledMap = Class.extend({
                     "numXTiles":    Math.floor(tsdata.imagewidth / that.tileSize.x),
                     "numYTiles":    Math.floor(tsdata.imageheight / that.tileSize.y)
                 };
-            that.tilesets.push(ts);
+            that.tileSets.push(ts);
         }
-    
     },
 
     //-----------------------------------------
     // Grabs tile from our 'layer' data and return the 'pkt' object for the layer we want to draw.
     // It takes as a parameter 'tileIndex', which is the id of the tile we'd like to draw in our layer data.
     getTilePacket: function (tileIndex) {
-        var that = this,
-            pkt = {
+        var that    = this,
+            tsets   = that.tileSets,
+            tset    = null,
+            tset_i  = 0,
+            pkt     = {
                 "img": null,        // The Image object of the given tile.
                 "px": 0, "py": 0    // The (x,y) values that we want to draw the tile to in map coordinates.
-            },
-            tile = 0;
-        for(tile = that.tilesets.length - 1; tile >= 0; tile--) {
-            if(that.tilesets[tile].firstgid <= tileIndex) break;
+            };
+        // Find the tileset that we want to draw
+        for(tset_i= tsets.length - 1; tset_i >= 0; tset_i--) {
+            // If the tileset's 'firstgid' parameter is less than the 'tileIndex' of the tile we want to draw,
+            // then we know that tile is not in the given tileset and we can skip to the next one.
+            if(tsets[tset_i].firstgid <= tileIndex){ tset = tsets[tset_i]; break; }
         }
-
+        if(tset===null) throw "No tileSet found for tileIndex:"+tileIndex;
         // Wet the 'img' parameter to the Image object of the appropriate 'tileset' found above.
-        pkt.img = that.tilesets[tile].image;
-
+        pkt.img = tset.image;
         // Calculate the position to draw to based on:
-        //
-        // 1) The local id of the tile
-        var localIdx = tileIndex - that.tilesets[tile].firstgid; //  calculated from the 'tileIndex' of the tile we want to draw and the 'firstgid' of the tileset we found earlier.
-
-        // 2) The (x,y) position of the tile in terms of the number of tiles in our tileset.
-        // Based on the 'numXTiles' of the given tileset. Note that 'numYTiles' isn't actually needed here. Think about how the tiles are arranged. 
-        var lTileX = Math.floor(localIdx % that.tilesets[tile].numXTiles);
-        var lTileY = Math.floor(localIdx / that.tilesets[tile].numXTiles);
-
-        // 3) the (x,y) pixel position in our tileset image of the tile we want to draw. 
-        // Based on the tile position we just calculated and the (x,y) size of each tile in pixels.
-        pkt.px = (lTileX * that.tileSize.x);
-        pkt.py = (lTileY * that.tileSize.y);
-
-
+        var localIdx = tileIndex - tset.firstgid,           //   1) The local id of the tile calculated from the 'tileIndex' of the tile we want to draw and the 'firstgid' of the tileset we found earlier.
+            lTileX = Math.floor(localIdx % tset.numXTiles), //   2) The (x,y) position of the tile in terms of the number of tiles in our tileset.                                                            
+            lTileY = Math.floor(localIdx / tset.numXTiles); //      Based on the 'numXTiles' of the given tileset. Note that 'numYTiles' isn't actually needed here. Think about how the tiles are arranged.                 
+        pkt.px = (lTileX * that.tileSize.x);                //   3) the (x,y) pixel position in our tileset image of the tile we want to draw. 
+        pkt.py = (lTileY * that.tileSize.y);                //      Based on the tile position we just calculated and the (x,y) size of each tile in pixels.
         return pkt;
     },
 
